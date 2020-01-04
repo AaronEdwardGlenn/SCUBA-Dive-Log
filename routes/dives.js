cf91ed59dc053a0a3378b622433ed735a1ea2892
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2020-01-04 12:35:02 -0800

    added dives.js routes

---
+++ routes/dives.js
@@ -0,0 +1,34 @@
+const { Router } = require('express');
+const Dives = require('../lib/modles/Dives');
+
+// eslint-disable-next-line new-cap
+module.exports = Router()
+  .post('/', (req, res) => {
+    Dives
+      .create(req.body)
+      .then(dive => res.send(dive));
+  })
+
+  .get('/', (req, res) => {
+    let diveQuery = {};
+    Dives
+      .find(diveQuery)
+      .select({ location: true })
+      .then(dives => res.send(dives));
+  })
+  .get('/:id', (req, res) => {
+    Promise.all([
+      Dives.findById(req.params.id)
+    ])
+      .then(([dive]) => res.send ({ ...dive.toJSON() }));
+  })
+  .patch('/id', (req, res) => {
+    Dives
+      .findByIdAndUpdate(req.params.id, req.body, { new: true })
+      .then(dive => res.send(dive));
+  })
+  .delete('/:id', (req, res) => {
+    Promise.all([Dives.findByIdAndDelete(req.params.id)])
+      .then(([dive]) => res.send(dive));
+   
+  });
